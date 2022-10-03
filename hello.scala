ac9d94dde1ad8251be2c633a7b014092b8cdefbb
Camilo Schaser-Hughes <camilo3@pdx.edu>
2022-10-02 20:04:50 -0700

    got it working on the psu server

--- hello.scala
+++ hello.scala
@@ -1,7 +1,13 @@
 
 object Hello {
 
-    def main(args: Array[String]): Unit = {
+    def main(argv: Array[String]) = {
         println("hello everyone")
     }
+
+    def scream() = {
+      println("AHHHHHH!!! WHAT AM i DOING!!!")
+      println("I HAVEN'T EVEN STARTED MAKING FLASH CARDS YET!! WTF!!!")
+    }
+
 }
